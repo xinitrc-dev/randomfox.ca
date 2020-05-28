937a9c05ec862bf8e48f99c73f47313a9a5cd2a4
FennecTECH <fennectech@gmail.com>
2020-05-27 21:18:41 -0500

    Update index.php

--- floof/index.php
+++ floof/index.php
@@ -3,11 +3,11 @@ header('Content-Type: application/json');
 
 $files = glob('../images/*');
 if ($files) {
-    $FOX_NUM = count($files);
+    $FOX_NUM = 121;
     $random_fox_index = rand(1, $FOX_NUM);
 
-    $image_path = 'https://randomfox.ca/images/'.$random_fox_index.'.jpg';
-    $link = 'https://randomfox.ca/?i='.$random_fox_index;
+    $image_path = 'https://github.com/xinitrc-dev/randomfox.ca/blob/master/images/'.$random_fox_index.'.jpg?raw=true';
+    $link = 'https://github.com/xinitrc-dev/randomfox.ca/blob/master/images/'.$random_fox_index'.jpg';
 } else {
     $image_path = null;
     $link = null;
